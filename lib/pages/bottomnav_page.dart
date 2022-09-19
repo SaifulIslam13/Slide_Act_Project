5bd4b3111b66f68e7d49ed6b7fb82e0a70d90246
Saiful Islam <saiful1561997@gmail.com>
2022-09-20 02:45:11 +0600

    Bottom Navbar

--- lib/pages/bottomnav_page.dart
+++ lib/pages/bottomnav_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
 class BottomNavPage extends StatefulWidget {
   const BottomNavPage({Key? key}) : super(key: key);
@@ -16,19 +17,54 @@ class _BottomNavPageState extends State<BottomNavPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text("Sign in as: " + user!.email!),
-          MaterialButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-            },
-            color: Colors.amber,
-            child: Text("Sign Out"),
-          )
-        ],
+      //bottom navbar
+      bottomNavigationBar: Container(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+          child: GNav(
+              activeColor: Colors.white,
+              backgroundColor: Colors.black,
+              color: Colors.grey.shade800,
+              tabBackgroundColor: Colors.grey.shade800,
+              padding: EdgeInsets.all(10),
+              gap: 8,
+              tabs: [
+                GButton(
+                  icon: Icons.home,
+                  text: "Home",
+                ),
+                GButton(
+                  icon: Icons.menu_book_sharp,
+                  text: "Qualification",
+                ),
+                GButton(
+                  icon: Icons.workspace_premium_rounded,
+                  text: "Experience",
+                ),
+                GButton(
+                  icon: Icons.person,
+                  text: "Profile",
+                ),
+              ]),
+        ),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Sign in as: " + user!.email!),
+            MaterialButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut();
+              },
+              color: Colors.amber,
+              child: Text("Sign Out"),
+            )
+          ],
+        ),
       ),
     );
   }
